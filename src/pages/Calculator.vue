f4187583624a01f894e091ed4028a9acb3ed8dac
Alina Carpio <agcarpio@espol.edu.ec>
2024-05-01 15:43:28 -0500

    Cambiar layout de Calculadora

--- src/pages/Calculator.vue
+++ src/pages/Calculator.vue
@@ -12,32 +12,32 @@ const mejoramiento = ref();
 
 <template>
   <div class="flex w-11/12 justify-end my-auto h-screen items-center">
-    <div class="w-1/2 flex-col self -center">
-      <div class="form flex flex-col">
-        <div class="flex mb-4 w-full">
-          <div class="w-1/3">
+    <div class="w-2/5 flex-col">
+      <div class="form flex flex-col w-full">
+        <div class="flex mb-4 w-full gap-4">
+          <div class="w-1/5">
             <label for="porcentajePractico" class="mr-3">% Práctico</label>
-            <InputText id="porcentajePractico" class="myTailwindInput" type="text"
+            <InputText id="porcentajePractico" class="w-full" type="text"
                        v-model="practPorcentaje"/>
           </div>
-          <div class="">
+          <div class="w-4/5">
             <label for="pract" class="mr-3">Nota práctica</label>
-            <InputText id="pract" type="text" v-model="practico"/>
+            <InputText id="pract" class="w-full" type="text" v-model="practico"/>
           </div>
         </div>
 
         <label for="parcial1">Primer Parcial</label>
-        <InputText id="parcial1" type="text" v-model="primerParcial"/>
+        <InputText id="parcial1"  class="mb-4" type="text" v-model="primerParcial"/>
 
         <label for="parcial2">Segundo Parcial</label>
-        <InputText id="parcial2" type="text" v-model="segundoParcial"/>
+        <InputText id="parcial2" class="mb-4" type="text" v-model="segundoParcial"/>
 
         <label for="mejora">Mejoramiento</label>
-        <InputText id="mejora" type="text" v-model="mejoramiento"/>
+        <InputText id="mejora" class="mb-4" type="text" v-model="mejoramiento"/>
 
       </div>
 
-      <Button label="Calcular" severity="success" raised/>
+      <Button label="Calcular" class="w-full" severity="success" raised/>
     </div>
 
 
@@ -46,7 +46,7 @@ const mejoramiento = ref();
 
 <style scoped>
 .myTailwindInput {
-  @apply bg-gray-100 dark:bg-gray-900 p-3 w-2/4 text-gray-900 dark:text-gray-50 rounded-full placeholder:text-gray-500 border border-transparent;
+  @apply bg-gray-100 dark:bg-gray-900 p-3 w-2/4 text-gray-900 dark:text-gray-50 rounded-xl shadow-inner placeholder:text-gray-500 border border-transparent;
 }
 
 </style>
