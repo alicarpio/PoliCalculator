57ee78167f1b347a0e1a9f873230dd4fc3f4722b
Alina Carpio <agcarpio@espol.edu.ec>
2024-05-06 13:19:46 -0500

    FEAT: Add custom InputNumber component

---
+++ src/components/InputNumber.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <form>
+    <input type="number" class="w-full bg-gray-100 dark:bg-gray-900 p-3 focus:outline-none focus:border-sky-400 focus:ring-1 focus:ring-sky-300 rounded-xl shadow-inner border border-slate-300" >
+  </form>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ResultNote.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+<div>
+  <img src="../assets/images/almost-done.svg">
+  <h1>Felicitaciones aprovastes </h1>
+  <h2>note</h2>
+  <h3>nota final</h3>
+  <small>Mejoramiento</small>
+</div>
+</template>
+
+<style scoped>
+
+</style>
