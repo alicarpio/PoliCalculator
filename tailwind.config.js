ecc19de6750e5d4d9a7ba9bcbed271eaf79e74f4
Alina Carpio <agcarpio@espol.edu.ec>
2024-05-09 22:38:17 -0500

    Change to light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  content: ["./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: "class", // or 'media' or 'class'
+    content: ["./node_modules/flowbite/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
 
